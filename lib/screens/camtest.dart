4f47b46c682316ecefd79e82792832d9f78f3fa2
Projjol <projjol@nyu.edu>
2019-09-29 07:49:10 -0400

    add camera feature

--- lib/screens/camtest.dart
+++ lib/screens/camtest.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class CamTest extends StatefulWidget {
 
@@ -21,6 +24,19 @@ class _CamTest extends State<CamTest> {
     if (isCamera){
       image = await ImagePicker.pickImage(source: ImageSource.camera);
       print(image.path);
+      final String endpoint = "https://a0040ba9.ngrok.io/check";
+      String base64Image = base64Encode(image.readAsBytesSync());
+      String fileName = image.path.split("/").last;
+      print(fileName);
+      http.post(endpoint, body: {
+        "image": base64Image,
+        "name": fileName,
+        "category": "Courant_paint"
+      }).then((res) {
+        print(res.statusCode);
+      }).catchError((err) {
+        print(err);
+      });
     } else {
       image = await ImagePicker.pickImage(source: ImageSource.gallery);
     }
@@ -28,6 +44,17 @@ class _CamTest extends State<CamTest> {
       _image = image;
     });
   }
+
+  // uploadFile() async {
+  //   var postUri = Uri.parse("<APIUrl>");
+  //   var request = new http.MultipartRequest("POST", postUri);
+  //   request.fields['user'] = 'blah';
+  //   request.files.add(new http.MultipartFile.fromBytes('file', await File.fromUri("<path/to/file").readAsBytes(), contentType: new MediaType('image', 'jpeg')))
+
+  //   request.send().then((response) {
+  //     if (response.statusCode == 200) print("Uploaded!");
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
